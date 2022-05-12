2fb72d650f119b8fe2f4f18ce9fc6681876726f1
monokumasix <kira.milka27@gmail.com>
2022-05-12 19:39:21 +0300

    tasc

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const numbersOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
-const obj {
+
+const personalMoviesDB {
     count: numbersOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+const a = prompt('Один из просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените?', ''),
+      c = prompt('Один из просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените?', '');
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB[c] = d;
+
+
+
+
